16986dca63b491104cb07084db35296cdc7db029
Seu107937 <seu107937@donga.edu.vn>
2025-12-01 15:58:15 +0700

    update

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ function addMessage(text, type) {
     div.className = `msg ${type}`;
     div.textContent = text;
     messagesBox.appendChild(div);
-    messagesBox.scrollTop = messagesBox.scrollHeight; // auto scroll
+    messagesBox.scrollTop = messagesBox.scrollHeight;
 }
 
 async function sendQuestion() {
@@ -16,30 +16,40 @@ async function sendQuestion() {
     addMessage(question, "user");
     input.value = "";
 
-    addMessage("Sơu đang trả lời...", "bot"); // loading tạm
+    addMessage("Sơu đang trả lời...", "bot");
     const loadingMsg = messagesBox.lastChild;
 
-    const res = await fetch("/ask", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ question })
-    });
-
-    const data = await res.json();
-
-    loadingMsg.textContent = data.answer;
+    try {
+        const res = await fetch("/ask", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ question })
+        });
+        const data = await res.json();
+        loadingMsg.textContent = data.answer;
+    } catch (e) {
+        loadingMsg.textContent = "Lỗi kết nối server!";
+    }
     messagesBox.scrollTop = messagesBox.scrollHeight;
 }
 
-// Gửi bằng Enter
-input.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") sendQuestion();
+
+input.addEventListener("focus", () => {
+    setTimeout(() => {
+      
+        input.scrollIntoView({ behavior: "smooth", block: "center" });
+        messagesBox.scrollTop = messagesBox.scrollHeight;
+    }, 300); 
 });
 
+
 const setHeight = () => {
+
     const height = window.visualViewport ? window.visualViewport.height : window.innerHeight;
+    
     document.body.style.height = `${height}px`;
     document.querySelector('.chat-container').style.height = `${height}px`;
+    window.scrollTo(0, 0); 
     
     messagesBox.scrollTop = messagesBox.scrollHeight;
 };
@@ -50,5 +60,9 @@ if (window.visualViewport) {
     window.addEventListener('resize', setHeight);
 }
 
-
 setHeight();
+
+// Gửi bằng Enter
+input.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") sendQuestion();
+});
